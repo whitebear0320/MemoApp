652854257e075d5e388b53b48a91bf9fe94b8e24
横溝大介 <yokomizodaisuke@yokomizodaisukenoMacBook-Pro.local>
2020-07-02 22:59:22 +0900

    Create Login and SignUp Screen

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import Appbar from './src/components/Appbar'
 // import MemoListScreen from './src/screens/MemoListScreen'
 // import MemoDetailScreen from './src/screens/MemoDetailScreen'
-import MemoEditScreen from './src/screens/MemoEditScreen'
+// import MemoEditScreen from './src/screens/MemoEditScreen'
+// import LoginScreen from './src/screens/LoginScreen'
+import SignUpScreen from './src/screens/SignUpScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Appbar/>
-      <MemoEditScreen/>
+      <SignUpScreen/>
     </View>
   );
 }
