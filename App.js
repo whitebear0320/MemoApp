1ecdca8cade0f1b0979741cb31e7ab03e2681156
横溝大介 <yokomizodaisuke@yokomizodaisukenoMacBook-Pro.local>
2020-07-06 23:47:33 +0900

    Install react-navigation

--- App.js
+++ App.js
@@ -1,30 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import Appbar from './src/components/Appbar'
-// import MemoListScreen from './src/screens/MemoListScreen'
-// import MemoDetailScreen from './src/screens/MemoDetailScreen'
-// import MemoEditScreen from './src/screens/MemoEditScreen'
-// import LoginScreen from './src/screens/LoginScreen'
+import MemoListScreen from './src/screens/MemoListScreen'
+import MemoDetailScreen from './src/screens/MemoDetailScreen'
+import MemoEditScreen from './src/screens/MemoEditScreen'
+import LoginScreen from './src/screens/LoginScreen'
 import SignUpScreen from './src/screens/SignUpScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Appbar/>
-      <SignUpScreen/>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fffdf6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 78,
+const App = createStackNavigator({
+  Home:       { screen: MemoListScreen },
+  MemoDetail: { screen: MemoDetailScreen },
+  MemoEdit:   { screen: MemoEditScreen },
+  Login:      { screen: LoginScreen },
+  Signup:     { screen: SignUpScreen },
+}, {
+  defaultNavigationOptions: {
+    headerTitle: 'Memot',
+    headerStyle: {
+      backgroundColor: '#265366',
+    },
+    headerTitleStyle: {
+      color: '#fff'
+    }
   },
-
 });
+
+export default createAppContainer(App);
--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, TouchableHighlight } from 'react-native'
 
 import * as Font from 'expo-font';
 import fontAwesome from '../../assets/fonts/fa-solid-900.ttf'
@@ -25,7 +25,7 @@ class CircleButton extends React.Component {
   }
 
   render() {
-    const {name, style, color} = this.props
+    const {name, style, color, onPress} = this.props
     let bgColor = '#e31676'
     let textColor = '#fff'
 
@@ -35,22 +35,28 @@ class CircleButton extends React.Component {
     }
 
     return(
-      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
-        {
-          this.state.fontLoaded? (
-            <CustomIcon name={name} style={[styles.circleButtonTitle, {color: textColor}]} />
-          ) : null
-        }
-      </View>
+      <TouchableHighlight style={[styles.container, style]} onPress={onPress} underlayColor="transparent">
+        <View style={[styles.circleButton, { backgroundColor: bgColor }]}>
+          {
+            this.state.fontLoaded? (
+              <CustomIcon name={name} style={[styles.circleButtonTitle, {color: textColor}]} />
+            ) : null
+          }
+        </View>
+      </TouchableHighlight>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  circleButton: {
+  container: {
+    width: 48, // 追加
+    height: 48,　// 追加
     position: 'absolute',
-    bottom: 32,
-    right: 32,
+    bottom: 24,
+    right: 24,
+  },
+  circleButton: {
     width: 48,
     height: 48,
     borderRadius: 24,
